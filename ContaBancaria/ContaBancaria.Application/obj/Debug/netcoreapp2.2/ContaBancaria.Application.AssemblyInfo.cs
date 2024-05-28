67410e70810a772eb62352050e8d320d1e6181be
Rafael Arantes <rafaeltwi@hotmail.com>
2024-05-28 14:30:26 -0300

    Merge

--- ContaBancaria/ContaBancaria.Application/obj/Debug/netcoreapp2.2/ContaBancaria.Application.AssemblyInfo.cs
+++ ContaBancaria/ContaBancaria.Application/obj/Debug/netcoreapp2.2/ContaBancaria.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContaBancaria.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e404868d1de184560f02a765c954b708a2de91")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContaBancaria.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContaBancaria.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
